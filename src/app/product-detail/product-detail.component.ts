a57242246d9df662c33d122ea9cdc284d2f09804
vinodprabhakar <vin62vic@gmail.com>
2019-05-01 20:26:41 +0530

    Update product-detail.component.ts

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../shared/product.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -7,14 +8,25 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
-
-  constructor(private _route: ActivatedRoute) { 
-    this._route.params.subscribe(params => {
-      console.log(params['id']);
-    });
+product: any;
+errorMSg : any;
+product_id : any;
+  constructor(private _route: ActivatedRoute,private _productService: ProductService,) { 
+    
   }
 
   ngOnInit() {
+this._route.params.subscribe(params => {
+      this.product_id=params['id'];
+    });
+    this._productService.getProducts().subscribe(products => { console.log(products);
+      this.product = products.filter(product => {  if(product.productId==this.product_id) return product;
+      })[0];
+      console.log(this.product);
+      
+    },
+      error => this.errorMSg = error
+    );
   }
 
 }
